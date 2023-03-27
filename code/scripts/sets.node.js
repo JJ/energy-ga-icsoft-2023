8eca0ccaa8f03c2d21f7aa8b618a07ae481b3918
JJ Merelo <jjmerelo@gmail.com>
2023-03-27 20:46:45 +0200

    Testing with 1024 takes a bit of time

--- code/scripts/sets.node.js
+++ code/scripts/sets.node.js
@@ -2,9 +2,19 @@ import { hashify, sacoUnion } from "saco-js";
 import { generateSets } from "../lib/utils.js";
 
 const size = process.argv[2];
-console.log(size);
 const NUMBER_OF_SETS = 1024;
 
 const sacos = generateSets(size, NUMBER_OF_SETS).map((s) => hashify(s));
 
-console.log(sacos);
+let merged = sacos;
+do {
+  merged = pairUnion(merged);
+} while (merged.length > 1);
+console.log(merged);
+
+function pairUnion(sacos) {
+  const byPairs = sacos.flatMap((_, i, a) =>
+    i % 2 ? [] : [a.slice(i, i + 2)]
+  );
+  return byPairs.map((p) => sacoUnion(p[0], p[1]));
+}
