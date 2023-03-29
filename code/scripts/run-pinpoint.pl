c89eb4b7840f4a4f32cb7c646434e17d88a66cc4
JJ Merelo <jjmerelo@gmail.com>
2023-03-29 20:41:38 +0200

    Includes also seconds

--- code/scripts/run-pinpoint.pl
+++ code/scripts/run-pinpoint.pl
@@ -13,7 +13,7 @@ my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
 open my $fh, ">", "data/pinpoint-sets-$suffix.csv";
-say $fh "Tool,Size,GPU,PKG";
+say $fh "Tool,size,GPU,PKG,seconds";
 
 for my $l ( qw(1024 2048 4096) ) {
   my $total_seconds;
@@ -29,7 +29,7 @@ for my $l ( qw(1024 2048 4096) ) {
       my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
       $total_seconds += $seconds;
       say "pinpoint, $l , $gpu ,$pkg";
-      push @results, [$gpu, $pkg];
+      push @results, [$gpu, $pkg,$seconds];
     }
   } while ( $successful < $ITERATIONS );
   my $average=$total_seconds/ $ITERATIONS;
@@ -43,7 +43,8 @@ for my $l ( qw(1024 2048 4096) ) {
     my $gpu_diff = $gpu_pkg[0] - $gpu;
     say $fh "pinpoint, $l, ",
       $gpu_diff > 0 ?$gpu_diff:0,  ", " ,
-      $gpu_pkg[1]-$pkg;
+      $gpu_pkg[1]-$pkg,
+      $gpu_pkg[2];
   }
 }
 close $fh;
