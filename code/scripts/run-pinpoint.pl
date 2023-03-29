74cfbd88e6355dbcc46f7edf97b0f5984e3e58e0
JJ Merelo <jjmerelo@gmail.com>
2023-03-29 20:37:58 +0200

    Computes the average time and substract closes #10
    It's capped at 0. It's a rough estimation, but it works

--- code/scripts/run-pinpoint.pl
+++ code/scripts/run-pinpoint.pl
@@ -8,15 +8,41 @@ use v5.14;
 use lib qw(lib ../lib);
 use Utils qw(%command_lines);
 
-my $successful = 0;
+my $ITERATIONS = 15;
+my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
+my $suffix = "$day-$mon-$hh-$mm-$ss";
 
-
-for my $l ( qw(1024) ) {
+open my $fh, ">", "data/pinpoint-$suffix.csv";
+for my $l ( qw(1024,2048,4096) ) {
+  say $fh "Tool,Size,GPU,PKG";
+  my $total_seconds;
+  my $successful = 0;
+  my @results;
   do {
     my $command = $command_lines{'deno'}." ". $l;
-    say $command;
+
     my $output = `pinpoint $command 2>&1`;
-    say $output;
-    $successful++;
-  } while ( $successful < 15 );
+    if ($output !~ /0.00\s+J/) {
+      $successful++;
+      my ( $gpu, $pkg ) = $output =~ /(\d+\.\d+)\s+J/g;
+      my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
+      $total_seconds += $seconds;
+      say "pinpoint, $l , $gpu ,$pkg";
+      push @results, [$gpu, $pkg];
+    }
+  } while ( $successful < $ITERATIONS );
+  my $average=$total_seconds/ $ITERATIONS;
+  my $baseline_output;
+  do {
+    $baseline_output = `pinpoint sleep $average 2>&1`;
+  } while ($baseline_output =~  /0.00\s+J/);
+  my ( $gpu, $pkg ) = $baseline_output =~ /(\d+\.\d+)\s+J/g;
+  foreach  my $row (@results) {
+    my @gpu_pkg = @$row;
+    my $gpu_diff = $gpu_pkg[0] - $gpu;
+    say $fh "pinpoint, $l, ",
+      $gpu_diff > 0 ?$gpu_diff:0,  ", " ,
+      $gpu_pkg[1]-$pkg;
+  }
 }
+close $fh;
