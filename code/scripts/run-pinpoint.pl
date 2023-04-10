45b01b0544f8ecd05d75a9e384c61f9249f0731d
JJ Merelo <jjmerelo@gmail.com>
2023-04-10 17:36:39 +0200

    Changing to test all vms

--- code/scripts/run-pinpoint.pl
+++ code/scripts/run-pinpoint.pl
@@ -12,39 +12,41 @@ my $ITERATIONS = 15;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
-open my $fh, ">", "data/pinpoint-sets-$suffix.csv";
-say $fh "Tool,size,GPU,PKG,seconds";
+open my $fh, ">", "data/pinpoint-vms-sets-$suffix.csv";
+say $fh "Tool,VM,size,GPU,PKG,seconds";
 
-for my $l ( qw(1024 2048 4096) ) {
-  my $total_seconds;
-  my $successful = 0;
-  my @results;
-  do {
-    my $command = $command_lines{'deno'}." ". $l;
+for my $c ( qw(node bun deno) ) {
+  for my $l ( qw(1024 2048 4096) ) {
+    my $total_seconds;
+    my $successful = 0;
+    my @results;
+    do {
+      my $command = $command_lines{$c}." ". $l;
 
-    my $output = `pinpoint $command 2>&1`;
-    if ($output !~ /0.00\s+J/) {
-      $successful++;
-      my ( $gpu, $pkg ) = $output =~ /(\d+\.\d+)\s+J/g;
-      my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
-      $total_seconds += $seconds;
-      say "pinpoint, $l , $gpu ,$pkg";
-      push @results, [$gpu, $pkg,$seconds];
+      my $output = `pinpoint $command 2>&1`;
+      if ($output !~ /0.00\s+J/) {
+        $successful++;
+        my ( $gpu, $pkg ) = $output =~ /(\d+\.\d+)\s+J/g;
+        my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
+        $total_seconds += $seconds;
+        say "pinpoint, $l , $gpu ,$pkg";
+        push @results, [$gpu, $pkg,$seconds];
+      }
+    } while ( $successful < $ITERATIONS );
+    my $average=$total_seconds/ $ITERATIONS;
+    my $baseline_output;
+    do {
+      $baseline_output = `pinpoint sleep $average 2>&1`;
+    } while ($baseline_output =~  /0.00\s+J/);
+    my ( $gpu, $pkg ) = $baseline_output =~ /(\d+\.\d+)\s+J/g;
+    foreach  my $row (@results) {
+      my @gpu_pkg = @$row;
+      my $gpu_diff = $gpu_pkg[0] - $gpu;
+      say $fh "pinpoint, $c, $l, ",
+        $gpu_diff > 0 ?$gpu_diff:0,  ", " ,
+        $gpu_pkg[1]-$pkg,", ",
+        $gpu_pkg[2];
     }
-  } while ( $successful < $ITERATIONS );
-  my $average=$total_seconds/ $ITERATIONS;
-  my $baseline_output;
-  do {
-    $baseline_output = `pinpoint sleep $average 2>&1`;
-  } while ($baseline_output =~  /0.00\s+J/);
-  my ( $gpu, $pkg ) = $baseline_output =~ /(\d+\.\d+)\s+J/g;
-  foreach  my $row (@results) {
-    my @gpu_pkg = @$row;
-    my $gpu_diff = $gpu_pkg[0] - $gpu;
-    say $fh "pinpoint, $l, ",
-      $gpu_diff > 0 ?$gpu_diff:0,  ", " ,
-      $gpu_pkg[1]-$pkg,", ",
-      $gpu_pkg[2];
   }
 }
 close $fh;
