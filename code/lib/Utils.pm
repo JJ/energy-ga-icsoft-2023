2ce79c75845fa3ba2e4f41c5a52807f5005862a3
JJ Merelo <jjmerelo@gmail.com>
2023-04-11 11:36:28 +0200

    Add results of onemax experiment

--- code/lib/Utils.pm
+++ code/lib/Utils.pm
@@ -3,7 +3,7 @@ use Exporter 'import';
 
 our @EXPORT_OK = qw( %command_lines );
 
-our %command_lines = ( deno => "/home/jmerelo/.deno/bin/deno run scripts/sets.deno.js",
-                     bun => "/home/jmerelo/.bun/bin/bun run scripts/sets.node.js",
-                     node => "/home/jmerelo/.nvm/versions/v18.0.0/bin/node scripts/sets.node.js" );
+our %command_lines = ( deno => "/home/jmerelo/.deno/bin/deno run scripts/",
+                     bun => "/home/jmerelo/.bun/bin/bun run scripts/",
+                     node => "/home/jmerelo/.nvm/versions/v18.0.0/bin/node scripts/" );
 
--- code/scripts/run-pinpoint.pl
+++ code/scripts/run-pinpoint.pl
@@ -8,20 +8,22 @@ use v5.14;
 use lib qw(lib ../lib);
 use Utils qw(%command_lines);
 
+my $script = shift || "sets";
 my $ITERATIONS = 15;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
-open my $fh, ">", "data/pinpoint-vms-sets-$suffix.csv";
+open my $fh, ">", "data/pinpoint-vms-$script-$suffix.csv";
 say $fh "Tool,VM,size,GPU,PKG,seconds";
 
 for my $c ( qw(node bun deno) ) {
+  my $infix = $c eq "deno" ? "deno": "node";
   for my $l ( qw(1024 2048 4096) ) {
     my $total_seconds;
     my $successful = 0;
     my @results;
     do {
-      my $command = $command_lines{$c}." ". $l;
+      my $command = $command_lines{$c}.$script.".".$infix.".js ". $l;
       say $command;
       my $output = `pinpoint $command 2>&1`;
       if ($output !~ /0.00\s+J/) {
