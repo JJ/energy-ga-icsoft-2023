3b856329cfe7b3009e940784229d721ea639a283
JJ Merelo <jjmerelo@gmail.com>
2023-05-29 17:45:07 +0200

    Preparing for #17

--- code/lib/Utils.pm
+++ code/lib/Utils.pm
@@ -5,5 +5,5 @@ our @EXPORT_OK = qw( %command_lines );
 
 our %command_lines = ( deno => "/home/jmerelo/.deno/bin/deno run scripts/",
                      bun => "/home/jmerelo/.bun/bin/bun run scripts/",
-                     node => "/home/jmerelo/.nvm/versions/v18.0.0/bin/node scripts/" );
+                     node => "/home/jmerelo/.nvm/versions/v18.16.0/bin/node scripts/" );
 
