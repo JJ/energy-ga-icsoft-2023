14b00bceee8a6111c6e3eca8785859f601713ca1
JJ Merelo <jjmerelo@gmail.com>
2023-05-31 13:25:29 +0200

    Use reasonable defaults

--- code/scripts/mutation.node.js
+++ code/scripts/mutation.node.js
@@ -1,6 +1,6 @@
 import { mutation, generateChromosomes } from "../lib/utils.js";
 
-const size = process.argv[2];
+const size = process.argv[2] || 1024;
 const NUMBER_OF_CHROMOSOMES = 40000;
 
 console.log("Size ", size);
