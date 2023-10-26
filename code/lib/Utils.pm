8226c71a71e4324b9530d92119daed03aa75aad7
JJ Merelo <jjmerelo@gmail.com>
2023-10-26 13:34:24 +0200

    #37 :white_check_mark: testing processing

--- code/lib/Utils.pm
+++ code/lib/Utils.pm
@@ -7,3 +7,15 @@ our %command_lines = ( deno => "/home/jmerelo/.deno/bin/deno run scripts/",
                      bun => "/home/jmerelo/.bun/bin/bun run scripts/",
                      node => "/home/jmerelo/.nvm/versions/node/v18.16.0/bin/node scripts/" );
 
+sub process_powermetrics_output {
+  my $output_file_name = shift;
+  my $content = do {
+    local $/ = undef;
+    open my $fh, "<", $output_file_name;
+    <$fh>;
+  };
+
+  # split content by lines like these *** Sampled system activity (Wed Oct 25 15:07:36 2023 +0200) (1031.24ms elapsed) ***
+    my @samples = split /\s+\*\*\* Sampled system activity/, $content;
+
+}
