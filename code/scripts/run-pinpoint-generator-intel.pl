0b1bfc1963666d0b256912b6799fcab4ba915860
JJ Merelo <jjmerelo@gmail.com>
2023-12-12 13:58:37 +0100

    Trying to fix it

--- code/scripts/run-pinpoint-generator-intel.pl
+++ code/scripts/run-pinpoint-generator-intel.pl
@@ -13,7 +13,7 @@ my $ITERATIONS = 15;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
-open my $fh, ">", "data/pinpoint-v2-$script-$suffix.csv";
+open my $fh, ">", "../../../data/pinpoint-v2-$script-$suffix.csv";
 say $fh "Tool,VM,size,GPU,PKG,seconds";
 
 my $infix = "node";
@@ -22,17 +22,17 @@ for my $l ( qw(512 1024 2048 ) ) {
   my $successful = 0;
   my @results;
   do {
-    my $command = "bun run chromosome_generation.node.js $l";
+    my $command = "bun run chromosome_creation.node.js $l";
     say $command;
     my $output = `pinpoint $command 2>&1`;
-    if ($output !~ /0.00\s+J/) {
+#    if ($output !~ /0.00\s+J/) {
       $successful++;
       my ( $gpu, $pkg ) = $output =~ /(\d+\.\d+)\s+J/g;
       my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
       $total_seconds += $seconds;
-      say "pinpoint, $c, $l , $gpu ,$pkg";
+      say "pinpoint, $l , $gpu ,$pkg";
       push @results, [$gpu, $pkg,$seconds];
-    }
+#    }
   } while ( $successful < $ITERATIONS );
 
   foreach  my $row (@results) {
