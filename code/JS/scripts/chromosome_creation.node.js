5261c297e6e47f6154765f8eff435b0cff28b024
JJ Merelo <jjmerelo@gmail.com>
2023-12-12 12:50:25 +0100

    :rotating_light: and fix workflow + :arrow_up:

--- code/JS/scripts/chromosome_creation.node.js
+++ code/JS/scripts/chromosome_creation.node.js
@@ -6,4 +6,3 @@ const NUMBER_OF_CHROMOSOMES = 40000;
 console.log("Size ", size);
 const allChromosomes = generateChromosomes(size, NUMBER_OF_CHROMOSOMES);
 console.log("Generated ", allChromosomes.length, " chromosomes");
-
