48f6dde0f2c057a75aaa8baa50f2c0edb03a89f3
JJ Merelo <jjmerelo@gmail.com>
2023-12-12 14:00:38 +0100

    Fixing CLI :bug:

--- code/scripts/run-pinpoint-generator-intel.pl
+++ code/scripts/run-pinpoint-generator-intel.pl
@@ -17,22 +17,23 @@ open my $fh, ">", "../../../data/pinpoint-v2-$script-$suffix.csv";
 say $fh "Tool,VM,size,GPU,PKG,seconds";
 
 my $infix = "node";
+my $bun_cli = "/home/jmerelo/.bun/bin/bun";
 for my $l ( qw(512 1024 2048 ) ) {
   my $total_seconds;
   my $successful = 0;
   my @results;
   do {
-    my $command = "bun run chromosome_creation.node.js $l";
+    my $command = "$bun_cli run chromosome_creation.node.js $l";
     say $command;
     my $output = `pinpoint $command 2>&1`;
-#    if ($output !~ /0.00\s+J/) {
+    if ($output !~ /0.00\s+J/) {
       $successful++;
       my ( $gpu, $pkg ) = $output =~ /(\d+\.\d+)\s+J/g;
       my ( $seconds ) = $output =~ /(\d+\.\d+) seconds/;
       $total_seconds += $seconds;
       say "pinpoint, $l , $gpu ,$pkg";
       push @results, [$gpu, $pkg,$seconds];
-#    }
+    }
   } while ( $successful < $ITERATIONS );
 
   foreach  my $row (@results) {
