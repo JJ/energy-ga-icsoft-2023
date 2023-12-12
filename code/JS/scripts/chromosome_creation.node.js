5602ceeb4c42b92f37e1d17457826480c963f675
JJ Merelo <jjmerelo@gmail.com>
2023-12-12 12:23:45 +0100

    Changing default size #57
    After checking that node, for some reason, breaks at 4096
    
    It's anyway a quite unrealistic size for evolutionary algorithms

--- code/JS/scripts/chromosome_creation.node.js
+++ code/JS/scripts/chromosome_creation.node.js
@@ -1,6 +1,6 @@
 import { generateChromosomes } from "../lib/utils.js";
 
-const size = process.argv[2] || 1024;
+const size = process.argv[2] || 512;
 const NUMBER_OF_CHROMOSOMES = 40000;
 
 console.log("Size ", size);
