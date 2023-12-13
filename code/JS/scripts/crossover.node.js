16a8c4e88ebeef12fa26cafeb0da13749a937c12
JJ Merelo <jjmerelo@gmail.com>
2023-12-13 08:32:07 +0100

    #58 it's taking around 1/3 of the time in the case of crossover

--- code/JS/scripts/crossover.node.js
+++ code/JS/scripts/crossover.node.js
@@ -4,7 +4,10 @@ const size = process.argv[2];
 const NUMBER_OF_CHROMOSOMES = 40000;
 
 console.log("Size ", size);
+const start = Date.now();
 const population = generateChromosomes(size, NUMBER_OF_CHROMOSOMES);
+const endGeneration = Date.now();
+console.warn("Time so far ", endGeneration - start);
 const pairs = [];
 const HALF_POPULATION = NUMBER_OF_CHROMOSOMES / 2;
 for (let i = 0; i < HALF_POPULATION; i++) {
@@ -16,5 +19,6 @@ const newPairs = [];
 pairs.forEach((pair) => {
   newPairs.push(crossover(pair[0], pair[1]));
 });
+console.warn("Time so far ", Date.now() - endGeneration);
 
 console.log(newPairs);
