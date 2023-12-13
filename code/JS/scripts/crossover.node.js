4a0a0fb5ae523a3ab94ee51d00db46439d8bbff9
JJ Merelo <jjmerelo@gmail.com>
2023-12-13 10:05:00 +0100

    #62 creating a whole generation in JS

--- code/JS/scripts/crossover.node.js
+++ code/JS/scripts/crossover.node.js
@@ -1,4 +1,4 @@
-import { crossover, generateChromosomes, mutation } from "../lib/utils.js";
+import { countOnes, crossover, generateChromosomes, mutation } from "../lib/utils.js";
 
 const size = process.argv[2];
 const NUMBER_OF_CHROMOSOMES = 40000;
@@ -10,14 +10,31 @@ const endGeneration = Date.now();
 console.warn("Time so far ", endGeneration - start);
 const pairs = [];
 const HALF_POPULATION = NUMBER_OF_CHROMOSOMES / 2;
+const selected = [];
 for (let i = 0; i < HALF_POPULATION; i++) {
-  pairs.push([population[i], population[i + HALF_POPULATION]]);
+  if (countOnes(population[i]) > countOnes(population[i + HALF_POPULATION])) {
+    selected.push(population[i]);
+  } else {
+    selected.push(population[i + HALF_POPULATION]);
+  }
 }
 
-const newPairs = [];
+// create random pairs of chromosomes in the selected array eliminating those that have already been paired
+while (selected.length > 0) {
+  const first = Math.floor(Math.random() * selected.length);
+  const firstChrom = selected[first];
+  selected.splice(first, 1);
+  const second = Math.floor(Math.random() * selected.length);
+  const secondChrom = selected[second];
+  selected.splice(second, 1);
+  pairs.push([firstChrom, secondChrom]);
+}
+
+const newGeneration = [];
 pairs.forEach((pair) => {
   const newPair = crossover(pair[0], pair[1]);
-  newPairs.push( [ mutation(newPair[0]), mutation(newPair[1]) ] );
+  newGeneration.push( mutation(newPair[0]));
+  newGeneration.push( mutation(newPair[1]));
 });
 console.warn("Time so far ", Date.now() - endGeneration);
 
