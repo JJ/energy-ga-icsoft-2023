2e681a8302036a6ae85adb950ff1b3967a01a7c5
JJ Merelo <jjmerelo@gmail.com>
2024-02-08 17:32:49 +0100

    :sparkles: #55

--- code/zig/build.zig
+++ code/zig/build.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.build) void {
+    const tests = b.addTest("src/my_library.zig");
+    tests.setBuildMode(.Debug);
 
-    const exe = b.addExecutable(.{
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(tests);
+
+    const target = b.Builder.standardTargetOptions(.{});
+    const optimize = b.Builder.standardOptimizeOption(.{});
+
+    const exe = b.Builder.addExecutable(.{
         .name = "onemax",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
---
+++ code/zig/src/chromosome_generator.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+
+fn countOnes(binaryString: []u8) u32 {
+    var count: u32 = 0;
+    for (binaryString) |binaryChar| {
+        count += binaryChar;
+    }
+    return count;
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    var argsIterator = try std.process.argsWithAllocator(allocator);
+    defer argsIterator.deinit();
+
+    _ = argsIterator.next(); // First argument is the program name
+
+    const stringLenArg = argsIterator.next().?;
+    const stringLength = try std.fmt.parseInt(u16, stringLenArg, 10);
+
+    const numStrings = 40000;
+
+    var i: u32 = 0;
+    const RndGen = std.rand.DefaultPrng;
+    var rnd = RndGen.init(0);
+
+    while (i < numStrings) {
+        i = i + 1;
+        var binaryString = try allocator.alloc(u8, stringLength);
+
+        var c: u32 = 0;
+        while (c < stringLength) : (c += 1) {
+            binaryString[c] = rnd.random().intRangeAtMost(u8, 0, 1);
+        }
+
+        const count = countOnes(binaryString);
+        _ = count;
+        _ = allocator.free(binaryString);
+    }
+}
