c36d379997694de15b50be8327419f7febcf0a9f
JJ Merelo <jjmerelo@gmail.com>
2024-02-08 13:13:19 +0100

    :white_check_mark: #55 #52 closes #49
    That issue closes since it's the first one that has succeeded

--- code/zig/src/generate.zig
+++ code/zig/src/generate.zig
@@ -1,27 +1,40 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 // function that generates a string array
-pub fn generate(stringLength: u16, numStrings: u32) ![]u8 {
-    const allocator = std.heap.page_allocator;
+pub fn generate(stringLength: u16, numStrings: u32) ![]*[]u8 {
+    var allocatorGenerator = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = allocatorGenerator.allocator();
+
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+
+    var stringArray: []*[]u8 = try allocator.alloc(*[]u8, numStrings);
 
     var i: u32 = 0;
-    const RndGen = std.rand.DefaultPrng;
-    var rnd = RndGen.init();
-
-    // Declare empty array to store the strings
-    var stringArray: []u8 = undefined;
-
     while (i < numStrings) {
-        i = i + 1;
         var binaryString = try allocator.alloc(u8, stringLength);
 
         var c: u32 = 0;
         while (c < stringLength) : (c += 1) {
-            binaryString[c] = rnd.random().intRangeAtMost(u8, 0, 1);
+            binaryString[c] = prng.random().intRangeAtMost(u8, 0, 1);
         }
 
         // add the new string to stringArray
-        stringArray = stringArray ++ binaryString;
+        stringArray[i] = &binaryString;
+        i = i + 1;
     }
     return stringArray;
+}
+
+// test the function
+test "generate" {
+    const stringLength: u16 = 10;
+    const numStrings: u32 = 10;
+    const stringArray = try generate(stringLength, numStrings);
+    try expect(stringArray[0].len == stringLength);
+    try expect(stringArray[numStrings - 1].len == stringLength);
 }
