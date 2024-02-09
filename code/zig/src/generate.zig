add9cb1b6f1f38b5f68e1eff0e1c1c8c867e10b0
JJ Merelo <jjmerelo@gmail.com>
2024-02-09 10:10:42 +0100

    :white_check_mark: check every element

--- code/zig/src/generate.zig
+++ code/zig/src/generate.zig
@@ -37,4 +37,13 @@ test "generate" {
     const stringArray = try generate(stringLength, numStrings);
     try expect(stringArray[0].len == stringLength);
     try expect(stringArray[numStrings - 1].len == stringLength);
+    // loop through all strings in StringArray to check that every element is either 1 or 0
+    var i: u32 = 0;
+    while (i < numStrings) {
+        var c: u16 = 0;
+        while (c < stringLength) : (c += 1) {
+            try expect(stringArray[i].*[c] == 0 or stringArray[i].*[c] == 1);
+        }
+        i = i + 1;
+    }
 }
