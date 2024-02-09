c696e1dbb4a5868a602e2ea47798d26471608c6e
JJ Merelo <jjmerelo@gmail.com>
2024-02-09 12:44:24 +0100

    closes #19 :recycle: and run data closes #55 already tested
    Also created dataset for the paper

--- code/scripts/run-pinpoint-zig.pl
+++ code/scripts/run-pinpoint-zig.pl
@@ -6,37 +6,36 @@ use warnings;
 use v5.14;
 
 use lib qw(lib ../lib ../../lib);
-use Utils qw(%command_lines process_pinpoint_output);
+use Utils qw(process_pinpoint_output);
 
 my $script = shift || "sets";
 my $ITERATIONS = 15;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
-open my $fh, ">", "../../data/pinpoint-v3-$script-$suffix.csv";
-say $fh "Tool,VM,size,GPU,PKG,seconds";
+open my $fh, ">", "../../data/pinpoint-zig-$suffix.csv";
+say $fh "VM,size,GPU,PKG,seconds";
 
-my $infix = "node";
 for my $l ( qw(512 1024 2048) ) {
   my $total_seconds;
   my $successful = 0;
   my @results;
   do {
-    my $command = $command_lines{"bun"}.$script.".".$infix.".js ". $l;
+    my $command = "zig-out/bin/chromosome_generator $l";
     say $command;
-    my ( $gpu, $pkg, $seconds ) = `pinpoint $command 2>&1`;
-    my @results = process_pinpoint_output $output;
+    my $output = `pinpoint $command 2>&1`;
+    my ( $gpu, $pkg, $seconds )= process_pinpoint_output $output;
     if ($gpu != 0 ) {
       $successful++;
       $total_seconds += $seconds;
-      say "pinpoint, $l, $gpu ,$pkg";
+      say "zig, $l, $gpu ,$pkg";
       push @results, [$gpu, $pkg,$seconds];
     }
   } while ( $successful < $ITERATIONS );
 
   foreach  my $row (@results) {
     say join(", ", @$row);
-    say $fh "pinpoint, bun, $l, ", join(", ", @$row);
+    say $fh "zig, $l, ", join(", ", @$row);
   }
 }
 close $fh;
