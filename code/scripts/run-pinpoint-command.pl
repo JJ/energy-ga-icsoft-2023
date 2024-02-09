37973867700efbeb7e53a66372c8cdc3971c21d9
JJ Merelo <jjmerelo@gmail.com>
2024-02-09 19:50:50 +0100

    closes #69 :coffin: a bit of explaining too

--- code/scripts/run-pinpoint-command.pl
+++ code/scripts/run-pinpoint-command.pl
@@ -9,13 +9,13 @@ use lib qw(lib ../lib ../../lib);
 use Utils qw(process_pinpoint_output);
 
 my $preffix = shift || die "I need a prefix for the data files";
-my $command = shift || die "I need a command to run";
+my $command = shift || die "I need a (single) command to run";
 my $ITERATIONS = 15;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
 open my $fh, ">", "../../data/pinpoint-$preffix-$suffix.csv";
-say $fh "VM,size,GPU,PKG,seconds";
+say $fh "Platform,size,GPU,PKG,seconds";
 
 for my $l ( qw(512 1024 2048) ) {
   my $total_seconds;
@@ -26,7 +26,7 @@ for my $l ( qw(512 1024 2048) ) {
     say $command;
     my $output = `pinpoint $command 2>&1`;
     say $output;
-    my ( $gpu, $pkg, $seconds )= process_pinpoint_output $output;
+    my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
     if ($gpu != 0 ) {
       $successful++;
       $total_seconds += $seconds;
