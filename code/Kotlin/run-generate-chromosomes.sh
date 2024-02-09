019181f18509ab0f13181c231c4bf05bbbcd49da
JJ Merelo <jjmerelo@gmail.com>
2024-02-09 17:39:59 +0100

    closes #50 Kotlin already started
    #51 adapting chromosome generation
    
    Created data for chromosome generation

---
+++ code/Kotlin/run-generate-chromosomes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+NUMBER_OF_CHROMOSOMES=$1
+java -jar generate_chromosomes.jar $NUMBER_OF_CHROMOSOMES
---
+++ code/scripts/run-pinpoint-command.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+
+use lib qw(lib ../lib ../../lib);
+use Utils qw(process_pinpoint_output);
+
+my $preffix = shift || die "I need a prefix for the data files";
+my $command = shift || die "I need a command to run";
+my $ITERATIONS = 15;
+my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
+my $suffix = "$day-$mon-$hh-$mm-$ss";
+
+open my $fh, ">", "../../data/pinpoint-$preffix-$suffix.csv";
+say $fh "VM,size,GPU,PKG,seconds";
+
+for my $l ( qw(512 1024 2048) ) {
+  my $total_seconds;
+  my $successful = 0;
+  my @results;
+  do {
+    my $command = "$command $l";
+    say $command;
+    my $output = `pinpoint $command 2>&1`;
+    say $output;
+    my ( $gpu, $pkg, $seconds )= process_pinpoint_output $output;
+    if ($gpu != 0 ) {
+      $successful++;
+      $total_seconds += $seconds;
+      say "$preffix, $l, $gpu ,$pkg";
+      push @results, [$gpu, $pkg,$seconds];
+    }
+  } while ( $successful < $ITERATIONS );
+
+  foreach  my $row (@results) {
+    say join(", ", @$row);
+    say $fh "$preffix, $l, ", join(", ", @$row);
+  }
+}
+close $fh;
