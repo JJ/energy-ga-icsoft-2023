4452053cd16b1a32771a9992c612c01e662b1510
Mario Garcia-Valdez <mariosky@gmail.com>
2024-02-15 10:18:16 -0800

    elixir mix

---
+++ code/elixir/genetic/lib/genetic.ex
@@ -0,0 +1,18 @@
+defmodule Genetic do
+  @moduledoc """
+  Documentation for `Genetic`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Genetic.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- code/elixir/scripts/crossover.exs
+++ code/elixir/scripts/crossover.exs
@@ -1,10 +1,11 @@
-defmodule Crossover do
+defmodule Genetic do
   def crossover_ind(p1, p2) do
     cx_point = :rand.uniform(length(p1))
     {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
     {h1 ++ t2, h2 ++ t1}
   end
 
+  # Move to another module
   def calculate(p1, p2, iterations) do
     prev = System.monotonic_time()
     for _ <- 1..iterations, do: crossover_ind(p1, p2)
