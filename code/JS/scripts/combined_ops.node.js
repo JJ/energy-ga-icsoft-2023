bd386b71adfbac253670c7874158ea525c3b2b05
JJ Merelo <jjmerelo@gmail.com>
2024-02-20 08:11:34 +0100

    closes #75 :sparkles:

---
+++ code/JS/scripts/combined_ops.node.js
@@ -0,0 +1,24 @@
+import { mutation, crossover, countOnes, generateChromosomes } from "../lib/utils.js";
+
+const size = process.argv[2] || 512;
+const NUMBER_OF_CHROMOSOMES = 40000;
+
+console.log("Size ", size);
+const population = generateChromosomes(size, NUMBER_OF_CHROMOSOMES);
+const pairs = [];
+const HALF_POPULATION = NUMBER_OF_CHROMOSOMES / 2;
+for (let i = 0; i < HALF_POPULATION; i++) {
+  pairs.push([population[i], population[i + HALF_POPULATION]]);
+}
+
+const newGeneration = [];
+pairs.forEach((pair) => {
+    const crossed_pair = crossover(pair[0], pair[1]);
+    for (let x in crossed_pair) {
+        const mutated = mutation(crossed_pair[x]);
+        newGeneration.push(mutated, countOnes(mutated));
+        console.log(crossed_pair[x], mutated, countOnes(mutated));
+    }
+});
+
+console.log(newPairs);
