3bb1243ce3fc154b8da3f04168971f9a05406fb8
JJ Merelo <jjmerelo@gmail.com>
2024-02-24 19:57:37 +0100

    Closes #78 refactoring done thanks to @dimdin in ziggit.dev
    https://ziggit.dev/t/error-expected-type-const-u8-found-u8/3352

--- code/zig/src/crossover.zig
+++ code/zig/src/crossover.zig
@@ -4,9 +4,9 @@ const ourRng = @import("utils.zig").ourRng;
 
 // Crossover operator that combines two strings, cutting them in two random points, interchanging the result
 pub fn crossover(allocator: std.mem.Allocator, random: std.rand.Random, binary_string_1: []u8, binary_string_2: []u8) !void {
-    var binary_string_1Clone: []u8 = try allocator.dupeZ(u8, binary_string_1);
+    var binary_string_1Clone = try allocator.dupeZ(u8, binary_string_1);
     defer allocator.free(binary_string_1Clone);
-    var binary_string_2Clone: []u8 = try allocator.dupeZ(u8, binary_string_2);
+    var binary_string_2Clone = try allocator.dupeZ(u8, binary_string_2);
     defer allocator.free(binary_string_2Clone);
 
     var index1 = random.int(u32) % binary_string_1.len;
@@ -41,6 +41,8 @@ test "crossover" {
     try expect(copy_binary_string_1.len == binary_string_1.len);
     try expect(copy_binary_string_2.len == binary_string_2.len);
     std.debug.print("binary_string_1: {s}\n", .{binary_string_1});
+    std.debug.print("binary_string_2: {s}\n", .{binary_string_2});
+
     try expect(!std.mem.eql(u8, copy_binary_string_1, binary_string_1));
     try expect(!std.mem.eql(u8, copy_binary_string_2, binary_string_2));
 }
