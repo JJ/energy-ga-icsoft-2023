c51a8e6a01b8fa385168b6678309fa8d9dae3b1d
Mario Garcia Valdez <mariosky@gmail.com>
2024-02-25 09:28:20 +0000

    config pom.xml can not run tests withm mvn test

---
+++ code/Kotlin/srcTests/kotlineoTest.kt
@@ -0,0 +1,41 @@
+import kotlineo.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class KotlineoTest {
+    @Test
+    fun testRandomChromosome() {
+        val length = 10
+        val actual = random_chromosome(length)
+
+        assertEquals(length, actual.size)
+        // every element should be either true or false
+        for (i in 0..length-1) {
+            assertTrue(actual[i] == true || actual[i] == false)
+        }
+    }
+
+
+    @Test
+    fun testHIFF() {
+        val subjects = mapOf(
+            "10" to 2,
+            "1100" to 8,
+            "1011" to 6,
+            "10101101100100" to 16,
+            "1010110110010011" to 22,
+            "010101101100100" to 19,
+            "00000000100000" to 42,
+            "1111111110000110" to 42,
+            "0010110100101101" to 24
+        )
+
+        var manyHIFF = ""
+        subjects.forEach { (input, expected) ->
+            manyHIFF += input
+            assertEquals(expected, hiff(input), "HIFF $input = $expected")
+        }
+        assertEquals(163, hiff(manyHIFF), "Many HIFF")
+    }
+}
