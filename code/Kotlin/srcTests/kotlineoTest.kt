2f35f0e6bfade35cf2e4d7394bce3a1f85f02e0f
JJ Merelo <jjmerelo@gmail.com>
2024-02-25 11:19:13 +0100

    closes #73, maven is already testing

--- code/Kotlin/srcTests/kotlineoTest.kt
+++ code/Kotlin/srcTests/kotlineoTest.kt
@@ -10,7 +10,6 @@ public class KotlineoTest {
         val actual = random_chromosome(length)
 
         assertEquals(length, actual.size)
-        // every element should be either true or false
         for (i in 0..length-1) {
             assertTrue(actual[i] == true || actual[i] == false)
         }
