76f31ac5bb3ec79e6339ece1a57b2a44c0061a94
Mario Garcia Valdez <mariosky@gmail.com>
2024-02-25 04:39:12 +0000

    kotlin hiff

---
+++ code/Kotlin/src/hiff.kt
@@ -0,0 +1,11 @@
+import kotlineo.*
+
+fun main(args : Array<String>) {
+
+
+    println("HIFF 2: " + hiff("10"));
+    println("HIFF 8: " + hiff("1100"));
+    println("HIFF 16: " + hiff("10101101100100"));
+    println("HIFF 42: " + hiff("00000000100000"));
+    println("HIFF 24: " + hiff("0010110100101101"));
+}
--- code/Kotlin/src/kotlineo.kt
+++ code/Kotlin/src/kotlineo.kt
@@ -62,6 +62,10 @@ fun draw(chromosome:BooleanArray){
   return;
 }
 
+fun f(ev: String): Int {
+    return if (ev == "0" || ev == "1") 1 else 0
+}
+
 fun t(a: String, b: String): String {
   if (a == b) {
     if (a == "0") {
@@ -74,11 +78,27 @@ fun t(a: String, b: String): String {
 }
 
 
+fun T(ev: String): String {
+    return when (ev) {
+        "-", "1", "0" -> ev
+        "00" -> "0"
+        "11" -> "1"
+        "01", "10" -> "-"
+        else -> if (ev.length == 2 && ev.contains("-")) "-" 
+        else t(T(ev.slice(0 until ev.length / 2)), T(ev.slice(ev.length / 2 until ev.length)))
+    }
+}
+
+
 fun hiff(stringChr:String): Int {
-when (stringChr) {
-    "0", "1" -> return 1
-    "00", "11" -> return 4
-    "01", "10" -> return 2
-    else -> return 10
+  println("hiff: " + stringChr);
+
+  return when (stringChr) {
+      "0", "1" -> 1
+      "00", "11" -> 4
+      "01", "10" -> 2
+      else -> stringChr.length * f(T(stringChr)) +
+                  hiff(stringChr.slice(0 until stringChr.length / 2)) +
+                  hiff(stringChr.slice(stringChr.length / 2 until stringChr.length))
   }
 }
