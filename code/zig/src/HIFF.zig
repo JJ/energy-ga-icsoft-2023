02bc4edbb2fca0eac35fdec19f415ceafcfd2b3d
JJ Merelo <jjmerelo@gmail.com>
2024-02-27 12:46:24 +0100

    Testing all over

--- code/zig/src/HIFF.zig
+++ code/zig/src/HIFF.zig
@@ -79,4 +79,6 @@ test "HIFF" {
     try expect(HIFF("1") == 1);
     try expect(HIFF("01") == 2);
     try expect(HIFF("00") == 4);
+    try expect(HIFF("1100") == 8);
+    try expect(HIFF("1111") == 8);
 }
