81286da5482e1c4f3f3a04cbd097eabab4bccc39
JJ Merelo <jjmerelo@gmail.com>
2024-03-06 10:13:51 +0100

    #67 boolean mutation

---
+++ code/zig/src/bool_mutation.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const ourRng = @import("utils.zig").ourRng;
+
+// mutation operator that changes a single random character in a string
+pub fn boolMutation(boolString: []bool, random: std.rand.Random) void {
+    var index = random.int(usize) % boolString.len;
+    boolString[index] = if (boolString[index] == true) false else true;
+}
+
+test "mutation" {
+    var boolString = [_]bool{ true, false, true, false, true, false };
+    var copyBoolString = [_]bool{ true, false, true, false, true, false };
+
+    var random = try ourRng();
+    boolMutation(&boolString, random.random());
+    try expect(copyBoolString.len == boolString.len);
+    std.debug.print("boolString: {any}\n", .{boolString});
+    try expect(!std.mem.eql(bool, &copyBoolString, &boolString));
+}
