3612274fbbd0b32594e6aa4b3ef755bd7d277dfa
JJ Merelo <jjmerelo@gmail.com>
2024-03-19 20:14:18 +0100

    #80 fixed in one case

--- code/zig/src/crossover.zig
+++ code/zig/src/crossover.zig
@@ -11,9 +11,7 @@ pub fn crossover(allocator: std.mem.Allocator, random: std.rand.Random, binary_s
 
     var index = random.int(u32) % (binary_string_1.len - 1);
     var len = 1 + random.int(u32) % (binary_string_1.len - index - 1);
-    std.debug.print("index: {d}, len: {d}\n", .{ index, len });
     for (index..index + len) |i| {
-        std.debug.print("i: {d}\n", .{i});
         binary_string_2[i] = binary_string_1Clone[i];
         binary_string_1[i] = binary_string_2Clone[i];
     }
@@ -22,23 +20,22 @@ pub fn crossover(allocator: std.mem.Allocator, random: std.rand.Random, binary_s
 test "crossover" {
     var prng = try ourRng();
     var allocator = std.testing.allocator;
+    const copy_binary_string_1 = try allocator.dupeZ(u8, "1010101");
+    defer allocator.free(copy_binary_string_1);
 
-    var binary_string_2 = try allocator.dupeZ(u8, "0101010");
-    defer allocator.free(binary_string_2);
     const copy_binary_string_2 = try allocator.dupeZ(u8, "0101010");
     defer allocator.free(copy_binary_string_2);
 
     for (0..1000) |_| {
         var binary_string_1 = try allocator.dupeZ(u8, "1010101");
         defer allocator.free(binary_string_1);
-        const copy_binary_string_1 = try allocator.dupeZ(u8, "1010101");
-        defer allocator.free(copy_binary_string_1);
+        var binary_string_2 = try allocator.dupeZ(u8, "0101010");
+        defer allocator.free(binary_string_2);
+
         try crossover(allocator, prng.random(), binary_string_1, binary_string_2);
 
         try expect(copy_binary_string_1.len == binary_string_1.len);
         try expect(copy_binary_string_2.len == binary_string_2.len);
-        std.debug.print("binary_string_1: {s}\n", .{binary_string_1});
-        std.debug.print("binary_string_2: {s}\n", .{binary_string_2});
 
         try expect(!std.mem.eql(u8, copy_binary_string_1, binary_string_1));
         try expect(!std.mem.eql(u8, copy_binary_string_2, binary_string_2));
