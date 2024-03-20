fb53dcaa37286a072161dbb1adf200c1206c6f45
JJ Merelo <jjmerelo@gmail.com>
2024-03-20 12:17:58 +0100

    #83 adapting runner

---
+++ code/scripts/run-pinpoint-command-bitset.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+
+use lib qw(lib ../lib ../../lib);
+use Utils qw(process_pinpoint_output);
+
+my $preffix = shift || die "I need a prefix for the data files";
+my $command = shift || die "I need a (single) command to run";
+my $ITERATIONS = 15;
+my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
+my $suffix = "$day-$mon-$hh-$mm-$ss";
+
+open my $fh, ">", "../../data/$preffix-$suffix.csv";
+say $fh "Platform,size,GPU,PKG,seconds";
+
+my $l = 2048;
+my $total_seconds;
+my $successful = 0;
+my @results;
+do {
+  my $output = `pinpoint $command 2>&1`;
+  say $output;
+  my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
+  if ($gpu != 0 ) {
+    $successful++;
+    $total_seconds += $seconds;
+    say "$preffix, $l, $gpu ,$pkg";
+    push @results, [$gpu, $pkg,$seconds];
+  }
+} while ( $successful < $ITERATIONS );
+
+foreach  my $row (@results) {
+  say join(", ", @$row);
+  say $fh "$preffix, $l, ", join(", ", @$row);
+}
+
+close $fh;
